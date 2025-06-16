70440c4a86d6706c1e088f555e275ab715d45f92
ํYingyot <yingyotn@gmail.com>
2025-06-16 17:24:42 +0700

    create webhook on repository 16Jun25

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post('/webhook', (req, res) => {
+    console.log('🔔 Webhook received:', JSON.stringify(req.body, null, 2));
+    res.sendStatus(200);
+});
+
+app.get('/', (req, res) => {
+    res.send('LINE Webhook is running!');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`🚀 Server is running on port ${PORT}`);
+});
